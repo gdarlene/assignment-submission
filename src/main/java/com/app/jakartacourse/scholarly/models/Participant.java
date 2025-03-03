bd709621edac94c66db16ed510befd63651829e7
gdarlene <darlyganza@gmail.com>
2025-03-03 14:54:24 +0200

    modified Paticipant.java by adding the core components of the java

--- src/main/java/com/app/jakartacourse/scholarly/models/Participant.java
+++ src/main/java/com/app/jakartacourse/scholarly/models/Participant.java
@@ -1,4 +1,95 @@
 package com.app.jakartacourse.scholarly.models;
 
 public class Participant {
+    private int id;
+    private String userName;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private String password;
+    private String Gender;
+    private String course;
+    private String phoneNumber;
+    private String classRoom;
+    Participant(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getClassRoom() {
+        return classRoom;
+    }
+
+    public void setClassRoom(String classRoom) {
+        this.classRoom = classRoom;
+    }
 }
