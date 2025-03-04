1ca2063069af788969fe8e6d571704cf1b314967
gdarlene <darlyganza@gmail.com>
2025-03-04 10:55:21 +0200

    added and modified files

--- src/main/java/com/app/jakartacourse/scholarly/controllers/StudentSignUpServlet.java
+++ src/main/java/com/app/jakartacourse/scholarly/controllers/StudentSignUpServlet.java
@@ -2,27 +2,35 @@ package com.app.jakartacourse.scholarly.controllers;
 
 import java.io.*;
 
+import com.app.jakartacourse.scholarly.models.Participant;
+import com.app.jakartacourse.scholarly.services.StudentService;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet("/signup")
 public class StudentSignUpServlet extends HttpServlet {
-    private String message;
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String userName = req.getParameter("userName");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String gender = req.getParameter("gender");
+        String phoneNum = req.getParameter("phoneNumber");
+        if (userName.isEmpty() || password.isEmpty() || email.isEmpty() || gender.isEmpty() || phoneNum.isEmpty()){
+            res.sendRedirect("studentSignup.jsp?error=MissingFields");
+        }
+        StudentService studentService = new StudentService();
+        if (studentService.getUserByEmail(email) != null){
+            res.sendRedirect("studentSignup.jsp?error=EmailAlreadyExists");
+            return;
+        }
+        Participant participant = new Participant();
+        participant.setUserName(userName);
+        participant.setPassword(password);
+        participant.setEmail(email);
+        participant.setGender(gender);
+        participant.setPhoneNumber(phoneNum);
 
-    public void init() {
-        message = "Hello World!";
-    }
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
+        studentService.saveUser(participant);
+        res.sendRedirect("signin.jsp?success=Success");
     }
 }
