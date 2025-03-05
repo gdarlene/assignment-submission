3a5c8efd9cdaadf82182ef2747feaf34daf27fc5
gdarlene <darlyganza@gmail.com>
2025-03-05 11:51:17 +0200

    modified files

--- src/main/java/com/app/jakartacourse/scholarly/models/Submissions.java
+++ src/main/java/com/app/jakartacourse/scholarly/models/Submissions.java
@@ -6,5 +6,46 @@ public class Submissions {
     private int id;
     private String filePath;
     private LocalDateTime submissionDate;
+    private Assignment assignment;
+    private UserRole student;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public LocalDateTime getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public void setSubmissionDate(LocalDateTime submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    public Assignment getAssignment() {
+        return assignment;
+    }
+
+    public void setAssignment(Assignment assignment) {
+        this.assignment = assignment;
+    }
+
+    public UserRole getStudent() {
+        return student;
+    }
+
+    public void setStudent(UserRole student) {
+        this.student = student;
+    }
 }
